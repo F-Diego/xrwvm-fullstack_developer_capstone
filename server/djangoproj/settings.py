7e7a3b16f11be380d62d21938c5c1f152ff67014
F-Diego <gar.fra@hotmail.com>
2026-07-21 12:47:31 -0400

    Implementación de vistas login, logout, registro y get_cars

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,9 +29,11 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'localhost','https://garfra-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai'
+    'localhost','https://garfra-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+                'https://garfra-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
 ]
-CSRF_TRUSTED_ORIGINS = ['https://garfra-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
+CSRF_TRUSTED_ORIGINS = ['https://garfra-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+                        'https://garfra-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
